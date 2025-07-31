9305d65ef1187e7481745b5b51934130a86364e4
Jorge Honorio <86316909+Jorgehnr@users.noreply.github.com>
2025-07-31 16:09:20 -0300

    Atualização da classe smartphone, nokia e iphone
    Atualização da classe smartphone, nokia e iphone com o código de solução do desafio.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,29 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }        
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
         }
 
-        public void Ligar()
+        public void Ligar(string discador)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Ligando... Numero Discado: " + discador);
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string identificador)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação... Numero identificado: " + identificador);
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
